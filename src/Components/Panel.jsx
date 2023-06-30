92c653bf8edb224d7642f9421c531cc5e55bf066
MapleLeafMakers <mapleleafmakers@gmail.com>
2023-06-30 15:09:32 -0400

    clear

--- src/Components/Panel.jsx
+++
@@ -1,7 +0,0 @@
-import { React } from 'react';
-import PropTypes from 'prop-types';
-import stylePropType from 'react-style-proptype';
-
-export default function Panel({ style, children }) { return <div style={style} className="panel">{children}</div>; }
-Panel.propTypes = { style: stylePropType, children: PropTypes.node.isRequired };
-Panel.defaultProps = { style: {} };
