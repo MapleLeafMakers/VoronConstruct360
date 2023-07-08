47bb1934859664f76fd8b97862a97a35043a0efd
MapleLeafMakers <mapleleafmakers@gmail.com>
2023-07-07 21:53:19 -0400

    update public path

--- quasar.config.js
+++ quasar.config.js
@@ -48,6 +48,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      'publicPath': 'VoronConstruct360',
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16',
